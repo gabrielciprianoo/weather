8f60ebf675e237e4fd27008f353c7411752c44a0
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-05-02 14:39:48 -0600

    ✨ styles and app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import styles from './App.module.css';
+import Form from './components/Form/Form';
+import Spinner from './components/Spinner/Spinner';
+import WeatherDetail from './components/WeatherDetail/WeatherDetail';
+import WeatherNotFound from './components/WeatherNotFound/WeatherNotFound';
+import useWeather from './hooks/useWeather';
 
 function App() {
+  
+  const { weather, fetchWeather, hasWeatherData, searching, notFound} = useWeather();
+
   return (
-    <>
+    <div>
       <h1 className={styles.title}>Buscador de Clima</h1>
       <div className={styles.container}>
-        <p>1</p>
-        <p>2</p>
+        <Form fetchWeather={fetchWeather} />
+        {searching && <Spinner/>}
+        {hasWeatherData && <WeatherDetail weather={weather} />}
+        {notFound && <WeatherNotFound />}
       </div>
-    </>
+    </div>
   );
 }
 
